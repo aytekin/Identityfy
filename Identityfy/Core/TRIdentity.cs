7ba1139687d4ec7c78281b82ce5bb17fbf3341b1
Aytekin Erlale <aytekinerlale@gmail.com>
2020-06-20 22:57:56 +0300

    Add project files.

---
+++ Identityfy/Core/TRIdentity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Identityfy.Core
+{
+    public static class TRIdentity
+    {
+        const int TRINO_LENGTH = 11;
+
+        /// <summary>
+        ///  This method validate a Turkey Republic Identity Number,
+        ///  TRINO : Turkey Republic Identity Number
+        /// </summary>
+        /// <param name="TRINO"> Turkey Republic Identity Number to validate </param>
+        /// <returns> bool </returns>
+        public static bool ValidateTRINO(string TRINO)
+        {
+            if (TRINO.Length != TRINO_LENGTH || TRINO[0] is '0') return false;
+
+            int odd, even, ninth, tenth;
+            odd = even = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 2 is 0)
+                    odd += GetNumericValue(TRINO[i]);
+                else
+                    even += GetNumericValue(TRINO[i]);
+            }
+
+            ninth = GetNumericValue(TRINO[9]);
+            tenth = GetNumericValue(TRINO[10]);
+
+            return (odd * 7 - even) % 10 == ninth ?
+                (odd + even + ninth) % 10 == tenth : false;
+        }
+
+        /// <summary>
+        ///  Generates a validate Turkey Republic Identity Number
+        /// </summary>
+        /// <returns> string </returns>
+        public static string GetRandomValidTRINO()
+        {
+            string result = "";
+            int odd, even, ninth, randomNumber;
+            odd = even = 0;
+
+            Random random = new Random();
+
+            randomNumber = random.Next(9) + 1;
+            result += randomNumber;
+            odd += randomNumber;
+
+            for (int i = 0; i < 8; i++)
+            {
+                randomNumber = random.Next(9);
+                result += randomNumber;
+
+                if (i % 2 is 0)
+                    even += randomNumber;
+                else
+                    odd += randomNumber;
+            }
+            ninth = (odd * 7 - even) % 10;
+            result += ninth;
+            result += (odd + even + ninth) % 10;
+
+            return result;
+        }
+
+        private static int GetNumericValue(char input)
+        {
+            return (int)Char.GetNumericValue(input);
+        }
+
+    }
+
+
+
+
+}
---
+++ UnitTest/Core/TRIdentityTest.cs
@@ -0,0 +1,77 @@
+﻿using Identityfy.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Core
+{
+    [TestClass]
+    public class TrIdentityTest
+    {
+        [TestMethod]
+        public void ValidateTRINO_ValidValue()
+        {
+            var result = TRIdentity.ValidateTRINO("75230734056");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateTRINO_InvalidValueNinthDigit()
+        {
+            var result = TRIdentity.ValidateTRINO("75230734016");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateTRINO_InvalidValueTenthDigit()
+        {
+            var result = TRIdentity.ValidateTRINO("75230734052");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateTRINO_ShortLengthValue()
+        {
+            var result = TRIdentity.ValidateTRINO("7523073");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateTRINO_LongLengthValue()
+        {
+            var result = TRIdentity.ValidateTRINO("75230734056921912");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateTRINO_FirstDigitZero()
+        {
+            var result = TRIdentity.ValidateTRINO("05230734056");
+
+            Assert.IsFalse(result);
+        }
+
+
+
+        [TestMethod]
+        public void GetRandomValidTRINO_GetResult()
+        {
+            var result = TRIdentity.GetRandomValidTRINO();
+            var validation = TRIdentity.ValidateTRINO(result);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(string));
+            Assert.IsTrue(validation);
+
+        }
+
+
+
+    }
+}
